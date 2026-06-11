0c37446a456958ed07ad8e911156913abed8de60
Devanshu Garg <mrdevanshugarg@gmail.com>
2026-06-11 18:16:19 +0530

    Add 2 no.

---
+++ 05Linked List/29_AddTwoNumber.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+//! Leetcode 2. Add Two Numbers LINK: https://leetcode.com/problems/add-two-numbers/submissions/2028104456/
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+
+        int carry = 0;
+        while(l1 && l2) {
+            int val = l1->val + l2->val + carry;
+            int digit = val % 10;
+
+            temp->next = new ListNode(digit);
+
+            carry = val / 10;
+
+            l1 = l1->next;
+            l2 = l2->next;
+            temp = temp->next;
+        }
+
+        while(l1) {
+            int val = l1->val + carry;
+            int digit = val % 10;
+
+            temp->next = new ListNode(digit);
+
+            carry = val / 10;
+
+            l1 = l1->next;
+            temp = temp->next;
+        }
+
+        while(l2) {
+            int val = l2->val + carry;
+            int digit = val % 10;
+
+            temp->next = new ListNode(digit);
+
+            carry = val / 10;
+
+            l2 = l2->next;
+            temp = temp->next;
+        }
+
+        if(carry) temp->next = new ListNode(carry);
+
+        return dummy->next;
+    }
+};
