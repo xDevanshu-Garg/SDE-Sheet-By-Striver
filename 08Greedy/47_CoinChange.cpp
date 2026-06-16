b2acffa0fd5eefbbda8eb05d2951bfec42ab1465
Devanshu Garg <mrdevanshugarg@gmail.com>
2026-06-16 18:48:14 +0530

    Coin Change

---
+++ 08Greedy/47_CoinChange.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+//! Leetcode 322. Coin Change LINK: https://leetcode.com/problems/coin-change/
+
+// ***************************************MEMOIZATION*************************************
+
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int solve(int i, int amount, vector<int>& coins) {
+        // Base Case
+        if(amount == 0) return 0;
+        if(i < 0) return 1e8;
+
+        if(dp[i][amount] != -1) return dp[i][amount];
+        
+        // Not pick: move to the previous coin
+        int ans  = solve(i-1, amount, coins);
+
+        // Pick: take the same coin again (unbounded knapsack)
+        if(coins[i] <= amount)  
+            ans = min(ans, 1 + solve(i, amount - coins[i], coins));
+        
+
+        return dp[i][amount] = ans;
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        dp.assign(n, vector<int>(amount+1, -1));
+
+        int ans = solve(n-1, amount, coins);
+        return (ans >= 1e8) ? -1: ans;
+    }
+};
+
+// ***************************************TABULATION*************************************
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<int> prev(amount+1, 1e8), curr(amount+1, 1e8);
+
+        //Base Case
+        for(int x = 0; x <= amount; x++) {
+            prev[x] = (x % coins[0] == 0) ? x/coins[0] : 1e8; 
+        }
+
+        for(int i = 1; i < n; i++) {
+            for(int x = 0; x <= amount; x++) {
+                int pick = 1e8;
+                if(x - coins[i] >= 0)
+                    pick = 1 + curr[x - coins[i]];
+
+                // notPick: move to the previous coin
+                int notPick = prev[x];
+                
+                curr[x] = min(pick, notPick);
+            }
+            prev = curr;
+        }
+
+        int ans = prev[amount];
+        return (ans >= 1e8) ? -1 : ans;
+    }
+};
+
+//***********************************************SPACE OPTIMIZED****************************************
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<int> prev(amount+1, 1e8), curr(amount+1, 1e8);
+
+        //Base Case
+        for(int x = 0; x <= amount; x++) {
+            prev[x] = (x % coins[0] == 0) ? x/coins[0] : 1e8; 
+        }
+
+        for(int i = 1; i < n; i++) {
+            for(int x = 0; x <= amount; x++) {
+                int pick = 1e8;
+                if(x - coins[i] >= 0)
+                    pick = 1 + curr[x - coins[i]];
+
+                // notPick: move to the previous coin
+                int notPick = prev[x];
+                
+                curr[x] = min(pick, notPick);
+            }
+            prev = curr;
+        }
+
+        int ans = prev[amount];
+        return (ans >= 1e8) ? -1 : ans;
+    }
+};
