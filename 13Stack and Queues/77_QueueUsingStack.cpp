513a5886dc8c6d3b0b6eb71d665635f53cf79db5
Devanshu Garg <mrdevanshugarg@gmail.com>
2026-06-24 09:57:24 +0530

    Implement Queue using Stack

---
+++ 13Stack and Queues/77_QueueUsingStack.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+//! Implement Queue using Stack LINK: https://leetcode.com/problems/implement-stack-using-queues/submissions/2045261595/
+class MyStack {
+public:
+    queue<int> q;
+    MyStack() {}
+    
+    void push(int x) {
+        q.push(x);
+
+        int sz = q.size();
+        for(int i = 0; i < sz - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        if(q.empty()) return -1;
+        int val = q.front(); q.pop();
+        return val;
+    }
+    
+    int top() {
+        if(q.empty()) return -1;
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
